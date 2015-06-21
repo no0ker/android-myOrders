4f4de1613e820fb522d139f710f2fa5cec8e9262
Рустам <rustam.shavaliev@gmail.com>
2015-06-21 16:35:02 +0300

    1

--- app/src/main/java/NetUtils/Resources/Messages.java
+++
@@ -1,7 +0,0 @@
-package NetUtils.Resources;
-
-public class Messages {
-    public static final String RELOAD_ORDERS_OK = "заявки обновлены";
-    public static final String RELOAD_ORDERS_NO = "заявок нету";
-    public static final String RELOAD_ORDERS_ERRROR = "ошибка при обновлении заявок";
-}
--- app/src/main/java/NetUtils/Resources/Resources.java
+++
@@ -1,7 +0,0 @@
-package NetUtils.Resources;
-
-public class Resources {
-    public static final String URL_DOMAIN = "http://csrvr.ru";
-    public static final String URL_MAIN = "http://csrvr.ru/applications/";
-    public static final String URL_ORDERS = "http://csrvr.ru/js/ajax/get-application.php";
-}
